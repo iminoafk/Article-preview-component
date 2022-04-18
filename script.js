2f273d79fe42234278c7995c9591ff00d5234213
iminoafk <73068623+iminoafk@users.noreply.github.com>
2022-04-18 21:30:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// séléctionne le bouton share
+const share = document.getElementById('share');
+
+// séléctionne la div share-info
+const shareInfo = document.getElementById('share-info');
+const d = document.getElementById('d');
+
+
+const matchResult = window.matchMedia("(max-width:1040px)");
+
+function isSmallScreen(){
+    return window.matchMedia("(max-width:1040px)").matches;
+}
+
+
+
+if (isSmallScreen() === true){
+    share.addEventListener('click', () => {  
+        shareInfo.classList.toggle('visible');
+        d.classList.toggle('hidden'); 
+    });
+} else {
+    share.addEventListener('click', () => {   
+        shareInfo.classList.toggle('hidden');
+    });
+}
+
